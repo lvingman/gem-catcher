8ff9ad36e570f9933adc535ba8f1c1ccfc76faf5
lvingman <r_martin@tutanota.com>
2025-07-19 15:45:08 -0300

    feat: first commit

---
+++ Scripts/Characters/Paddle.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class Paddle : CharacterBody2D
+{
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _Process(double delta)
+    {
+        
+    }
+}
